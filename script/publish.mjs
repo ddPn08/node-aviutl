77f4d765b4ff55c36704f32b6320a4ac9239036a
ddPn08 <pyn.g.1125@gmail.com>
2022-05-05 11:27:25 +0900

    fix: fix publish script

--- script/publish.mjs
+++ script/publish.mjs
@@ -6,26 +6,29 @@ import { WORKSPACES } from './__setup.mjs'
 
 const dryRun = process.argv.includes('--dry-run')
 
-const getTag = () => {
+/**
+ * @param {string} cwd
+ * @returns {Record<string, string> | undefined}
+ */
+const getTag = (cwd) => {
     try {
-        const tagList = execSync(`yarn npm tag list --json`)
-        return JSON.parse(tagList.toString()).locator
+        const tagList = execSync(`yarn npm tag list --json`, { cwd })
+        return JSON.parse(tagList.toString()).locator.split('@').pop()
     } catch (error) {
         return
     }
 }
 
 WORKSPACES.forEach(async (module) => {
-    const dir = path.join(__dirname, '../modules', module)
-    process.chdir(dir)
+    const cwd = path.join(__dirname, '../modules', module)
 
-    const { name, version } = JSON.parse(await fs.promises.readFile(path.join(dir, 'package.json'), 'utf8'))
-    const latest = getTag()
-
+    const { name, version } = JSON.parse(await fs.promises.readFile(path.join(cwd, 'package.json'), 'utf8'))
+    const latest = getTag(cwd)
+    
     if (latest && version === latest) {
         console.log(`${name}@${version} already published`)
     } else {
-        if (!dryRun) execSync(`yarn npm publish --access public --tag latest`, { stdio: 'inherit' })
+        if (!dryRun) execSync(`yarn npm publish --access public --tag latest`, { stdio: 'inherit', cwd })
         console.log(`${name}@${version} published`)
     }
 })
